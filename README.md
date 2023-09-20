eb48fe5cd4130ddf240fbc5b4de8d5e025a79fc0
Shaquib Khan <shaquibkhanrdx34@gmail.com>
2023-09-20 22:54:01 +0530

    complete code

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# real-estate-app
+# real-estate-starter
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+  return <footer className='bg-black py-8 text-center text-white'>
+  <div className='container max-auto '>
+    
+    Copyrigth &copy;2023. All rights reserved. </div></footer>;
+};
+
+export default Footer;
